7105164a995f86d6806190248ebf2d8b50145500
Stephen <stephen_h369@live.co.uk>
2020-09-10 11:50:01 +0100

    show route for course controller

--- src/main/java/com/codeclan/example/course_booking_system/controllers/CourseController.java
+++ src/main/java/com/codeclan/example/course_booking_system/controllers/CourseController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -19,5 +20,10 @@ public class CourseController {
     @GetMapping(value = "/courses")
     public ResponseEntity<List<Course>> getAllCourses() {
         return new ResponseEntity<>(courseRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/courses/{id}")
+    public ResponseEntity<List<Course>> getCoursesById(@PathVariable Long id) {
+        return new ResponseEntity(courseRepository.findById(id), HttpStatus.OK);
     }
 }
