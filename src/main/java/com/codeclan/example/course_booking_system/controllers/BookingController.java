e2fce53aab20775d96ad832ee4bf601e99eca799
Stephen <stephen_h369@live.co.uk>
2020-09-10 11:48:45 +0100

    show route for booking controller

--- src/main/java/com/codeclan/example/course_booking_system/controllers/BookingController.java
+++ src/main/java/com/codeclan/example/course_booking_system/controllers/BookingController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -19,5 +20,10 @@ public class BookingController {
     @GetMapping(value = "/bookings")
     public ResponseEntity<List<Booking>> getAllBookings() {
         return new ResponseEntity<>(bookingRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/bookings/{id}")
+    public ResponseEntity<List<Booking>> getBookingsById(@PathVariable Long id) {
+        return new ResponseEntity(bookingRepository.findById(id), HttpStatus.OK);
     }
 }
