87e0dd771438979d32d87b9e78d4be54937dab32
Stephen <stephen_h369@live.co.uk>
2020-09-10 11:35:13 +0100

    customer controller getAll

--- src/main/java/com/codeclan/example/course_booking_system/controllers/CustomerController.java
+++ src/main/java/com/codeclan/example/course_booking_system/controllers/CustomerController.java
@@ -1,8 +1,14 @@
 package com.codeclan.example.course_booking_system.controllers;
 
+import com.codeclan.example.course_booking_system.models.Customer;
 import com.codeclan.example.course_booking_system.repositories.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class
@@ -10,4 +16,9 @@ CustomerController {
 
     @Autowired
     CustomerRepository customerRepository;
+
+    @GetMapping(value = "/customers")
+    public ResponseEntity<List<Customer>> getAllCustomers() {
+        return new ResponseEntity<>(customerRepository.findAll(), HttpStatus.OK);
+    }
 }
