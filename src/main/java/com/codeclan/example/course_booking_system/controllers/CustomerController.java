3963a86c9a8ecce5e7663f39f5978192461a4f55
Stephen <stephen_h369@live.co.uk>
2020-09-10 11:47:00 +0100

    show route customer controller

--- src/main/java/com/codeclan/example/course_booking_system/controllers/CustomerController.java
+++ src/main/java/com/codeclan/example/course_booking_system/controllers/CustomerController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -20,5 +21,10 @@ CustomerController {
     @GetMapping(value = "/customers")
     public ResponseEntity<List<Customer>> getAllCustomers() {
         return new ResponseEntity<>(customerRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/customers/{id}")
+    public ResponseEntity<List<Customer>> getCustomerById(@PathVariable Long id) {
+        return new ResponseEntity(customerRepository.findById(id), HttpStatus.OK);
     }
 }
