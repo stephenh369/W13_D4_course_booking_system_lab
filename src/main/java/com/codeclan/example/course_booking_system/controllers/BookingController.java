1a6529771ab7d90558b693c42a61a5818f5a21f2
Stephen <stephen_h369@live.co.uk>
2020-09-10 11:39:07 +0100

    booking controller getAll

--- src/main/java/com/codeclan/example/course_booking_system/controllers/BookingController.java
+++ src/main/java/com/codeclan/example/course_booking_system/controllers/BookingController.java
@@ -1,4 +1,23 @@
 package com.codeclan.example.course_booking_system.controllers;
 
+import com.codeclan.example.course_booking_system.models.Booking;
+import com.codeclan.example.course_booking_system.repositories.BookingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class BookingController {
+
+    @Autowired
+    BookingRepository bookingRepository;
+
+    @GetMapping(value = "/bookings")
+    public ResponseEntity<List<Booking>> getAllBookings() {
+        return new ResponseEntity<>(bookingRepository.findAll(), HttpStatus.OK);
+    }
 }
